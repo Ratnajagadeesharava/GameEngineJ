0125cdba702c711a53e1da9f6ca1fa8ca2b84393
Ratna Jagadeesh <35951109+Ratnajagadeesharava@users.noreply.github.com>
2024-03-12 23:18:08 +0530

    added premake 5 as build system

---
+++ premake5.lua
@@ -0,0 +1,99 @@
+workspace "Mango"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Mango"
+	location "Mango"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/3rdparty/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines	{
+			"MG_PLATFORM_WINDOWS",
+			"MG_BUILD_DLL"
+		}
+
+		postbuildcommands{
+			'{COPY} "%{cfg.buildtarget.relpath}" "../bin/' .. outputdir .. '/Sandbox"'
+		}
+
+	filter "configurations:Debug"
+		defines "MG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "MG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs	{
+		"Mango/3rdparty/spdlog/include",
+		"Mango/src"
+	}
+
+	links{
+		"Mango"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines	{
+			"MG_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "MG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "MG_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "MG_DIST"
+		optimize "On"
